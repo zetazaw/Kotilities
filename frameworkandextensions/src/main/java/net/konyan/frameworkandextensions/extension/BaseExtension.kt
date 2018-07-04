a8b286f2c71b2c26a1d9900839b86716e10e1c4f
big9 <developer.big9@gmail.com>
2018-07-04 17:14:21 +0630

    add some more function on base extension

--- frameworkandextensions/src/main/java/net/konyan/frameworkandextensions/extension/BaseExtension.kt
+++ frameworkandextensions/src/main/java/net/konyan/frameworkandextensions/extension/BaseExtension.kt
@@ -1,17 +1,30 @@
 package net.konyan.frameworkandextensions.extension
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.annotation.IdRes
 import android.support.v4.app.*
 import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
 import android.text.TextUtils.replace
+import android.text.TextWatcher
 import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
 import net.konyan.frameworkandextensions.BuildConfig
 import net.konyan.frameworkandextensions.framework.BasePresenter
 import java.security.MessageDigest
+
+
+//toast
+
+fun Any.toast(context: Context, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(context, this.toString(), length).show()
+}
+
 
 private fun loggin(tag: String, message: String, level: String){
     if (BuildConfig.DEBUG){
@@ -87,6 +100,19 @@ inline fun <reified T: Activity> Fragment.navigate(requestCode: Int = 0x007, key
     startActivityForResult(intent, requestCode)
 }
 
+//special extension
+
+fun EditText.onChange(cb: (String) -> Unit){
+    this.addTextChangedListener(object : TextWatcher {
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            cb(s.toString())
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+        override fun afterTextChanged(s: Editable?) { }
+    })
+}
 
 //hasing
 
