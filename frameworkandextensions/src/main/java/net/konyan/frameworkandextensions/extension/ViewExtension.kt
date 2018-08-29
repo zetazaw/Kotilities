8a4ae6157107f6449a6c363ac1bde2bc6673d2b8
Ko Nyan <zetazaw@gmail.com>
2018-08-29 14:28:54 +0630

    update editText change listener
    listen input when user type in edit text

--- frameworkandextensions/src/main/java/net/konyan/frameworkandextensions/extension/ViewExtension.kt
+++ frameworkandextensions/src/main/java/net/konyan/frameworkandextensions/extension/ViewExtension.kt
@@ -22,3 +22,15 @@ fun <ITEM> RecyclerView.setup(items: List<ITEM>,
         setHasFixedSize(setFix)
     }
 }
+
+fun EditText.onChange(cb: (String) -> Unit){
+    this.addTextChangedListener(object : TextWatcher {
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            cb(s.toString())
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+        override fun afterTextChanged(s: Editable?) { }
+    })
+}
